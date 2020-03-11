22f6f022d6f20556d0a8feb1af1513fbd6d45318
LENOVO <olzhaskhozhakhmed@gmail.com>
2020-03-11 15:46:55 +0600

    add Login

--- app/src/main/java/com/example/bilimordahotel/LoginActivity.kt
+++ app/src/main/java/com/example/bilimordahotel/LoginActivity.kt
@@ -1,12 +1,54 @@
 package com.example.bilimordahotel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
+    private val TAG = "LoginActivity"
+
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        Log.d(TAG, "onCreate: ")
+
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
+        login_btn.setOnClickListener(this)
+        mAuth = FirebaseAuth.getInstance()
+
     }
+    override fun onClick(view: View) {
+        val email = email_input.text.toString()
+        val password = password_input.text.toString()
+        if(validate(email, password)){
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                if (it.isSuccessful){
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+            }
+
+        }
+    }
+
+    private fun validate(email: String, password: String)=
+        email.isNotEmpty() &&  password.isNotEmpty()
+    override fun afterTextChanged(p0: Editable?) {
+        login_btn.isEnabled = validate(email_input.text.toString(), password_input.text.toString())
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 }
--- app/src/main/java/com/example/bilimordahotel/MainActivity.kt
+++ app/src/main/java/com/example/bilimordahotel/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.bilimordahotel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
 
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.addAuthStateListener {
+            if(it.currentUser==null){
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
+        sign_out_text.setOnClickListener {
+            mAuth.signOut()
+        }
+}
+    override fun onStart(){
+        super.onStart()
+        if (mAuth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
     }
 }
